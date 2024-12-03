33205b1ef5eea47b14d14779d5e6523651d37ff9
Ali-Bh-yahya <zadanz.112233@gmail.com>
2024-12-03 13:56:03 +0200

    Java assignment

---
+++ JAVA/JAVA_Stack/Java_Fundamentals/Greetings/First_Java_Program/JavaPrint.java
@@ -0,0 +1,7 @@
+public  class JavaPrint{
+  public static void main(String[] args){
+    System.out.println("Ali Yahya");
+    System.out.println("I am 21 years old");
+    System.out.println("I am from Palestine");
+  }
+}
