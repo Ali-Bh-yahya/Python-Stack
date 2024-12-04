889f586eff7d6594859e095140e07f5c383a25c4
Ali-Bh-yahya <zadanz.112233@gmail.com>
2024-12-04 13:55:09 +0200

    delete Java Stack

--- JAVA/JAVA_Stack/Java_Fundamentals/Greetings/First_Java_Program/JavaPrint.java
+++
@@ -1,7 +0,0 @@
-public  class JavaPrint{
-  public static void main(String[] args){
-    System.out.println("Ali Yahya");
-    System.out.println("I am 21 years old");
-    System.out.println("I am from Palestine");
-  }
-}
